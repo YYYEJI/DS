a0b7a5f0565fe91ccc9ba7bcebad6bcc97d31aad
YYYEJI <22100396@handong.ac.kr>
2022-04-30 21:16:45 -0700

    file example

---
+++ file.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// ifstream inStream;    // input 전용 stream
+// ofstream outStream;   // output 전용 stream
+// file 선언하면 file open은 자동으로 수행
+
+int main(){
+    ifstream f1("a.dat");
+    ofstream f2("b.dat");   
+    int a, b;
+    if(!f1) 
+        cout<<"File open error: the file does not exist.\n";
+    else{
+        f1>>a>>b;
+        f2<<a*b;
+        f1.close();
+        f2.close();
+    }
+}
