4693ce9849f4057694b1e89af7a47e428a5673a0
YYYEJI <22100396@handong.ac.kr>
2022-06-12 00:36:58 -0700

    Serach algorithms

--- search.cpp
+++ search.cpp
@@ -21,25 +21,25 @@ int main(){
     cout<<"\nScore: "<<student[key].key<<endl;
     return 0;
 }
-int sequential_search(element a[], int k, int n){
+int sequential_search(element a[], int k, int n){     // array, key, array_size
     int j = 0;
-    while((j<n)&&(a[j].key!=k))
-        j++;
-    if(j>=n)
+    while((j<n)&&(a[j].key!=k))      // array 사이즈보다 j가 작음, j번째 key가 찾는 key값과 다르면 
+        j++;                         // index+1
+    if(j>=n)                         // array 사이즈를 j가 넘으면 찾는 key 값 없음
         return -1;
-    return j;
+    return j;                        // 찾았으면 index 값 return 
 }
 int binary_search(element a[], int k, int left, int right){
     int middle;
     while(left<=right){
-        middle = (left+right)/2;
+        middle = (left+right)/2;     // array size의 중간 값
 
-        if(a[middle].key==k)
+        if(a[middle].key==k)         // key 찾으면 index return 
             return middle;
-        if(a[middle].key>k)
+        if(a[middle].key>k)          // 찾는 key 값이 작으면 array 반으로 자르기 (앞쪽)
             right = middle-1;
-        if(a[middle].key<k)
+        if(a[middle].key<k)          // 찾는 key 값이 작으면 array 반으로 자르기 (뒷쪽)
             left = middle+1;
     }
-    return -1;
+    return -1;                       // 못 찾으면 -1 return 
 }
